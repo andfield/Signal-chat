c374ae1cd3050e5c9ddabbf0d03752161823ce4c
andfield <siddharththakur76@gmail.com>
2021-03-30 14:11:35 +1300

    Firebase setup

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCcscxjxz6EMC0hJS4O27CddlItAXCzXlY",
+    authDomain: "signal-clone-30add.firebaseapp.com",
+    projectId: "signal-clone-30add",
+    storageBucket: "signal-clone-30add.appspot.com",
+    messagingSenderId: "67536949646",
+    appId: "1:67536949646:web:275b5b516208ceaf8cd4df"
+  };
