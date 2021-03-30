ddfb9e1085dec604264463ba0fe7dd02ec6380a6
andfield <siddharththakur76@gmail.com>
2021-03-30 15:57:34 +1300

    Logut fuction added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,27 +1,47 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from 'react-native'
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import CustomListItem from '../components/CustomListItem';
+import { SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import CustomListItem from "../components/CustomListItem";
+import { Avatar } from "react-native-elements";
+import { auth, db } from "../firebase";
+import { TouchableOpacity } from "react-native";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            title: "Signal"
-        })
-    }, [])
+// signout function
+const signOutUser = () => {
+    auth.signOut().then(() => {
+        navigation.replace('Login')
+    })
+} 
 
-    return (
-        <SafeAreaView>
-            <StatusBar style='light' />
-            <ScrollView>
-                <CustomListItem />
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Signal",
+      headerStyle: { backgroundColor: "white" },
+      headerTitleStyle: { color: "black", marginLeft: 96 },
+      headerTintColor: "black",
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
+            <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+          </TouchableOpacity>
+        </View>
+      ),
+    });
+  }, []);
 
-export default HomeScreen
+  return (
+    <SafeAreaView>
+      <StatusBar style="light" />
+      <ScrollView>
+        <CustomListItem />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
