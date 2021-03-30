7b422d81a945d067efd4767f5f12f6ca1d64cd85
andfield <siddharththakur76@gmail.com>
2021-03-30 15:34:13 +1300

    HomeScreen Created

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import CustomListItem from '../components/CustomListItem';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Signal"
+        })
+    }, [])
+
     return (
-        <View>
-            <Text>This is the Home Page</Text>
-        </View>
+        <SafeAreaView>
+            <StatusBar style='light' />
+            <ScrollView>
+                <CustomListItem />
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
