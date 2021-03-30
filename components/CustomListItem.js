df46f1e7a7ee3e17f37756e432f02981091f5f5a
andfield <siddharththakur76@gmail.com>
2021-03-30 21:23:15 +1300

    lessgoo

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { ListItem, Avatar } from "react-native-elements";
+import { db } from "../firebase";
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+  //states
+  const [chatMessages, setChatMessages] = useState([]);
+
+  //Use effect to get all the messages.
+  useEffect(() => {
+    const unsubscribe = db
+      .collection("chats")
+      .doc(id)
+      .collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setChatMessages(snapshot.docs.map((doc) => doc.data()))
+      );
+    return unsubscribe;
+  }, []);
+
   return (
     <ListItem key={id} bottomDivider onPress={() => enterChat(id, chatName)}>
       <Avatar
         rounded
         source={{
           uri:
-            // chatMessages?.[0]?.photoURL ||
+            chatMessages?.[0]?.photoURL ||
             "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
         }}
       />
@@ -22,7 +39,7 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
           {chatName}
         </ListItem.Title>
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-          This is a coding chat
+          {chatMessages?.[0]?.displayName} : {chatMessages?.[0]?.message}
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
